7da337218157ed8183f3fc49a9f5fa9150de5fe5
thmim <mdtaharimmim1010@gmail.com>
2025-10-02 14:26:04 +0600

    development branch created

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="font-bold">Hello
-    
+    <div className="font-bold">
+    <h1 className="text-5xl font-bold">Hello</h1>
     </div>
     
   );
