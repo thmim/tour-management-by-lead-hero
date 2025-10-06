24809bb18ccd6341948829b78e25daf0af94d141
thmim <mdtaharimmim1010@gmail.com>
2025-10-06 21:37:47 +0600

    added server link

--- src/Hooks/axiosPublic.js
+++ src/Hooks/axiosPublic.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const useAxiosPublic = () => {
   const instance = axios.create({
-    baseURL: "http://localhost:5000",
-    // baseURL: `https://tour-management-project-server-by-l-omega.vercel.app`,
+    // baseURL: "http://localhost:5000",
+    baseURL: `https://tour-management-project-server-by-l-omega.vercel.app`,
 
   });
 
